f36dcc45b991960ee1d400b4280015ede329be3d
Imran Khan <m_imran_khan@me.com>
2017-09-07 09:54:43 +0200

    list of beverages + details returned

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,15 +5,30 @@ export class Beverage {
   name: string;
 }
 
+const BEVERAGES: Beverage[] = [
+  { id: 11, name: 'Coffee: Americano' },
+  { id: 12, name: 'Coffee: Cappucino' },
+  { id: 13, name: 'Coffee: Caffé Latte' },
+  { id: 14, name: 'Coffee: Espresso' },
+  { id: 15, name: 'Coffee: Ristretto' },
+  { id: 16, name: 'Tea: Earl grey' },
+  { id: 17, name: 'Tea: Lemon' },
+  { id: 18, name: 'Tea: Ginger' },
+  { id: 19, name: 'Tea: Regular' },
+  { id: 20, name: 'Tea: Green' }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'have a hot beverage!';
-  beverage: Beverage = {
-    id: 1,
-    name: 'Dark roast'
-  };
+  title = 'have a hot beverage';
+  beverages = BEVERAGES;
+  selectedBeverage: Beverage;
+
+  onSelect(beverage: Beverage): void {
+    this.selectedBeverage = beverage;
+  }
 }
